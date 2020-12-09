4678af4aa382893f1ddaef8624a76a2690663196
wb-shf786711 <wb-shf786711@alibaba-inc.com>
2020-12-09 17:20:23 +0800

    国际化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <a-locale-provider :locale="locale">
     <router-view />
+    </a-locale-provider>
   </div>
 </template>
 
 <script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      locale: zhCN
+    }
+  }
 }
 </script>
 
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>首页</div>
+  <div class="calendar">
+    <div>
+      <h4>快看日历</h4>
+    </div>
+    <a-calendar @panelChange="onPanelChange" />
+  </div>
 </template>
 
 <script>
 export default {
-}
+  methods: {
+    onPanelChange(value, mode) {
+      console.log(value, mode);
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="less">
+.calendar {
+  height: auto;
+}
 </style>
